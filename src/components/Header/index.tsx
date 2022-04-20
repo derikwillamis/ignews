e04b9b28d525b1b886487854f341dd4bdff56742
derikwillamis <clachgigante@gmail.ocmn>
2022-04-20 10:09:04 -0300

    novo

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss'
+
+
+export function Header() {
+    return (
+        <header className={styles.headercontainer}>
+            <div className={styles.headercontent}>
+                <img src="/images/logo.svg" alt="ig.news"/>
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
