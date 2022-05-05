e4879f3cef9a0113b09f243219f67dc1327f479e
derikwillamis <clachgigante@gmail.ocmn>
2022-05-05 10:02:50 -0300

    novo

--- src/pages/api/subcribe.ts
+++ src/pages/api/subcribe.ts
@@ -1,14 +1,43 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import {query as q } from 'faunadb'
 import { getSession } from "next-auth/client";
+import { fauna } from "../../servicer/fauna";
 import { stripe } from "../../servicer/stripe";
+
+
+type user = {
+    ref:{
+        id:string
+    }
+}
 
 export default async (req:NextApiRequest,res:NextApiResponse) => {
     if(req.method === 'POST'){
        const session = await getSession({ req });
 
+       const users = await fauna.query<user>(
+           q.Get(
+               q.Match(
+                   q.Index('users_by_email'),
+                   q.Casefold(session.user.email)
+               )
+           )
+       )
+
        const stripeCustomer= await stripe.customers.create({
          email: session.user.email,
        })
+
+       await fauna.query(
+           q.Update(
+               q.Ref(q.Collection('users'), users.ref.id),
+               {
+                   data:{
+                       stripe_Customer_id:stripeCustomer.id,
+                   }
+               }
+           )
+       )
 
        const stripeCheckoutSession = await stripe.checkout.sessions.create({
             customer:stripeCustomer.id,
